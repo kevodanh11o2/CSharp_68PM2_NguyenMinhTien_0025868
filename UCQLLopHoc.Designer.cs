e04321d2ebd164c5206fede8f2782eb1667d2b16
kevodanh11o2 <ocshitcho@gmail.com>
2026-06-15 00:12:50 +0700

    feat<lophoc>: CRUD lophoc

--- UCQLLopHoc.Designer.cs
+++ UCQLLopHoc.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace CSharp_68PM2_NguyenMinhTien_0025868
+namespace CSharp_68PM2_NguyenMinhTien_0025868
 {
     partial class UCQLLopHoc
     {
@@ -62,12 +62,13 @@
             this.btnLast.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLast.ForeColor = System.Drawing.Color.DimGray;
             this.btnLast.Location = new System.Drawing.Point(808, 652);
-            this.btnLast.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLast.Margin = new System.Windows.Forms.Padding(2);
             this.btnLast.Name = "btnLast";
             this.btnLast.Size = new System.Drawing.Size(56, 53);
             this.btnLast.TabIndex = 46;
             this.btnLast.Text = ">>";
             this.btnLast.UseVisualStyleBackColor = false;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
             // 
             // btnNext
             // 
@@ -76,12 +77,13 @@
             this.btnNext.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnNext.ForeColor = System.Drawing.Color.DimGray;
             this.btnNext.Location = new System.Drawing.Point(754, 652);
-            this.btnNext.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnNext.Margin = new System.Windows.Forms.Padding(2);
             this.btnNext.Name = "btnNext";
             this.btnNext.Size = new System.Drawing.Size(56, 53);
             this.btnNext.TabIndex = 45;
             this.btnNext.Text = ">";
             this.btnNext.UseVisualStyleBackColor = false;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
             // 
             // btnPrev
             // 
@@ -90,12 +92,13 @@
             this.btnPrev.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnPrev.ForeColor = System.Drawing.Color.DimGray;
             this.btnPrev.Location = new System.Drawing.Point(444, 652);
-            this.btnPrev.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnPrev.Margin = new System.Windows.Forms.Padding(2);
             this.btnPrev.Name = "btnPrev";
             this.btnPrev.Size = new System.Drawing.Size(56, 53);
             this.btnPrev.TabIndex = 44;
             this.btnPrev.Text = "<";
             this.btnPrev.UseVisualStyleBackColor = false;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
             // 
             // lblTrang
             // 
@@ -114,12 +117,13 @@
             this.btnFirst.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnFirst.ForeColor = System.Drawing.Color.DimGray;
             this.btnFirst.Location = new System.Drawing.Point(390, 652);
-            this.btnFirst.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnFirst.Margin = new System.Windows.Forms.Padding(2);
             this.btnFirst.Name = "btnFirst";
             this.btnFirst.Size = new System.Drawing.Size(56, 53);
             this.btnFirst.TabIndex = 42;
             this.btnFirst.Text = "<<";
             this.btnFirst.UseVisualStyleBackColor = false;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
             // 
             // label1
             // 
@@ -131,16 +135,14 @@
             this.label1.Size = new System.Drawing.Size(82, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Mã  lớp học:";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(19, 155);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(315, 25);
             this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label2
             // 
@@ -156,7 +158,7 @@
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(19, 257);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(315, 25);
             this.textBox2.TabIndex = 3;
@@ -173,14 +175,13 @@
             this.grbThongTinSinhVien.Controls.Add(this.label1);
             this.grbThongTinSinhVien.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.grbThongTinSinhVien.Location = new System.Drawing.Point(8, 63);
-            this.grbThongTinSinhVien.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.grbThongTinSinhVien.Margin = new System.Windows.Forms.Padding(2);
             this.grbThongTinSinhVien.Name = "grbThongTinSinhVien";
-            this.grbThongTinSinhVien.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.grbThongTinSinhVien.Padding = new System.Windows.Forms.Padding(2);
             this.grbThongTinSinhVien.Size = new System.Drawing.Size(352, 463);
             this.grbThongTinSinhVien.TabIndex = 33;
             this.grbThongTinSinhVien.TabStop = false;
             this.grbThongTinSinhVien.Text = "Thông tin Lớp học";
-            this.grbThongTinSinhVien.Enter += new System.EventHandler(this.grbThongTinSinhVien_Enter);
             // 
             // label4
             // 
@@ -196,10 +197,11 @@
             // textBox4
             // 
             this.textBox4.Location = new System.Drawing.Point(19, 75);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(315, 25);
             this.textBox4.TabIndex = 6;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
             // label3
             // 
@@ -215,7 +217,7 @@
             // textBox3
             // 
             this.textBox3.Location = new System.Drawing.Point(19, 349);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(315, 25);
             this.textBox3.TabIndex = 4;
@@ -227,12 +229,13 @@
             this.btnThem.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnThem.ForeColor = System.Drawing.Color.White;
             this.btnThem.Location = new System.Drawing.Point(8, 496);
-            this.btnThem.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnThem.Margin = new System.Windows.Forms.Padding(2);
             this.btnThem.Name = "btnThem";
             this.btnThem.Size = new System.Drawing.Size(165, 45);
             this.btnThem.TabIndex = 34;
             this.btnThem.Text = "Thêm";
             this.btnThem.UseVisualStyleBackColor = false;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
             // 
             // btnXoa
             // 
@@ -241,12 +244,13 @@
             this.btnXoa.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnXoa.ForeColor = System.Drawing.Color.White;
             this.btnXoa.Location = new System.Drawing.Point(8, 561);
-            this.btnXoa.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnXoa.Margin = new System.Windows.Forms.Padding(2);
             this.btnXoa.Name = "btnXoa";
             this.btnXoa.Size = new System.Drawing.Size(165, 45);
             this.btnXoa.TabIndex = 35;
             this.btnXoa.Text = "Xóa";
             this.btnXoa.UseVisualStyleBackColor = false;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
             // 
             // btnSua
             // 
@@ -255,12 +259,13 @@
             this.btnSua.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSua.ForeColor = System.Drawing.Color.White;
             this.btnSua.Location = new System.Drawing.Point(196, 496);
-            this.btnSua.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSua.Margin = new System.Windows.Forms.Padding(2);
             this.btnSua.Name = "btnSua";
             this.btnSua.Size = new System.Drawing.Size(165, 45);
             this.btnSua.TabIndex = 36;
             this.btnSua.Text = "Sửa";
             this.btnSua.UseVisualStyleBackColor = false;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
             // 
             // btnLamMoi
             // 
@@ -269,12 +274,13 @@
             this.btnLamMoi.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLamMoi.ForeColor = System.Drawing.Color.White;
             this.btnLamMoi.Location = new System.Drawing.Point(196, 561);
-            this.btnLamMoi.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLamMoi.Margin = new System.Windows.Forms.Padding(2);
             this.btnLamMoi.Name = "btnLamMoi";
             this.btnLamMoi.Size = new System.Drawing.Size(165, 45);
             this.btnLamMoi.TabIndex = 37;
             this.btnLamMoi.Text = "Làm mới";
             this.btnLamMoi.UseVisualStyleBackColor = false;
+            this.btnLamMoi.Click += new System.EventHandler(this.btnLamMoi_Click);
             // 
             // lblTimKiem
             // 
@@ -290,7 +296,7 @@
             // txtTimKiem
             // 
             this.txtTimKiem.Location = new System.Drawing.Point(381, 45);
-            this.txtTimKiem.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTimKiem.Margin = new System.Windows.Forms.Padding(2);
             this.txtTimKiem.Name = "txtTimKiem";
             this.txtTimKiem.Size = new System.Drawing.Size(260, 20);
             this.txtTimKiem.TabIndex = 39;
@@ -302,12 +308,13 @@
             this.btnTim.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnTim.ForeColor = System.Drawing.Color.White;
             this.btnTim.Location = new System.Drawing.Point(660, 33);
-            this.btnTim.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnTim.Margin = new System.Windows.Forms.Padding(2);
             this.btnTim.Name = "btnTim";
             this.btnTim.Size = new System.Drawing.Size(85, 36);
             this.btnTim.TabIndex = 40;
             this.btnTim.Text = "Tìm";
             this.btnTim.UseVisualStyleBackColor = false;
+            this.btnTim.Click += new System.EventHandler(this.btnTim_Click);
             // 
             // button1
             // 
@@ -316,7 +323,7 @@
             this.button1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.Location = new System.Drawing.Point(110, 625);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(165, 45);
             this.button1.TabIndex = 47;
@@ -328,12 +335,13 @@
             this.dgv_DSLH.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
             this.dgv_DSLH.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgv_DSLH.Location = new System.Drawing.Point(381, 74);
-            this.dgv_DSLH.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgv_DSLH.Margin = new System.Windows.Forms.Padding(2);
             this.dgv_DSLH.Name = "dgv_DSLH";
             this.dgv_DSLH.RowHeadersWidth = 51;
             this.dgv_DSLH.RowTemplate.Height = 24;
             this.dgv_DSLH.Size = new System.Drawing.Size(679, 492);
             this.dgv_DSLH.TabIndex = 48;
+            this.dgv_DSLH.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_DSLH_CellClick);
             this.dgv_DSLH.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_DSLH_CellContentClick);
             // 
             // UCQLLopHoc
@@ -355,9 +363,10 @@
             this.Controls.Add(this.btnXoa);
             this.Controls.Add(this.btnThem);
             this.Controls.Add(this.grbThongTinSinhVien);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "UCQLLopHoc";
             this.Size = new System.Drawing.Size(1082, 751);
+            this.Load += new System.EventHandler(this.UCQLLopHoc_Load);
             this.grbThongTinSinhVien.ResumeLayout(false);
             this.grbThongTinSinhVien.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgv_DSLH)).EndInit();
--- UCQLLopHoc.cs
+++ UCQLLopHoc.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,24 +13,255 @@ namespace CSharp_68PM2_NguyenMinhTien_0025868
     public partial class UCQLLopHoc : UserControl
     {
         databaseDataContext db = new databaseDataContext();
+        
+        private int pageSize = 10;
+        private int currentPage = 1;
+        private int totalPage = 1;
+        private string searchKeyword = "";
+
         public UCQLLopHoc()
         {
             InitializeComponent();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void UCQLLopHoc_Load(object sender, EventArgs e)
         {
-
+            textBox4.ReadOnly = true;
+            LoadData();
         }
 
-        private void grbThongTinSinhVien_Enter(object sender, EventArgs e)
+        private void LoadData()
         {
+            databaseDataContext db = new databaseDataContext();
+            var query = db.LopHocs.AsQueryable();
 
+            if (!string.IsNullOrEmpty(searchKeyword))
+            {
+                query = query.Where(lh =>
+                    lh.malop.ToLower().Contains(searchKeyword) ||
+                    lh.tenlop.ToLower().Contains(searchKeyword) ||
+                    (lh.ghichu != null && lh.ghichu.ToLower().Contains(searchKeyword))
+                );
+            }
+
+            int totalRecords = query.Count();
+            totalPage = (int)Math.Ceiling((double)totalRecords / pageSize);
+            if (totalPage == 0) totalPage = 1;
+
+            if (currentPage > totalPage) currentPage = totalPage;
+            if (currentPage < 1) currentPage = 1;
+
+            lblTrang.Text = $"Trang {currentPage}/{totalPage}";
+
+            dgv_DSLH.DataSource = query
+                .Skip((currentPage - 1) * pageSize)
+                .Take(pageSize)
+                .Select(lh => new {
+                    lh.id,
+                    lh.malop,
+                    lh.tenlop,
+                    lh.ghichu
+                })
+                .ToList();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void btnThem_Click(object sender, EventArgs e)
         {
+            string maLop = textBox1.Text.Trim();
+            string tenLop = textBox2.Text.Trim();
+            string ghiChu = textBox3.Text.Trim();
 
+            if (string.IsNullOrEmpty(maLop) || string.IsNullOrEmpty(tenLop))
+            {
+                MessageBox.Show("Vui lòng điền đầy đủ Mã lớp học và Tên lớp học.", "Cảnh báo",
+                                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (db.LopHocs.Any(lh => lh.malop.ToLower() == maLop.ToLower()))
+            {
+                MessageBox.Show("Mã lớp học này đã tồn tại.", "Lỗi",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                LopHoc lh = new LopHoc
+                {
+                    malop = maLop,
+                    tenlop = tenLop,
+                    ghichu = ghiChu
+                };
+
+                db.LopHocs.InsertOnSubmit(lh);
+                db.SubmitChanges();
+                MessageBox.Show("Thêm lớp học thành công!", "Thông báo",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                btnLamMoi_Click(sender, e);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi khi thêm: {ex.Message}", "Lỗi",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox4.Text, out int id))
+            {
+                MessageBox.Show("Vui lòng chọn một lớp học để sửa.", "Cảnh báo",
+                                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string maLop = textBox1.Text.Trim();
+            string tenLop = textBox2.Text.Trim();
+            string ghiChu = textBox3.Text.Trim();
+
+            if (string.IsNullOrEmpty(maLop) || string.IsNullOrEmpty(tenLop))
+            {
+                MessageBox.Show("Vui lòng điền đầy đủ Mã lớp học và Tên lớp học.", "Cảnh báo",
+                                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (db.LopHocs.Any(lh => lh.id != id && lh.malop.ToLower() == maLop.ToLower()))
+            {
+                MessageBox.Show("Mã lớp học này đã được sử dụng bởi lớp khác.", "Lỗi",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                LopHoc lh = db.LopHocs.FirstOrDefault(l => l.id == id);
+                if (lh == null)
+                {
+                    MessageBox.Show("Không tìm thấy lớp học trong cơ sở dữ liệu.", "Lỗi",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                lh.malop = maLop;
+                lh.tenlop = tenLop;
+                lh.ghichu = ghiChu;
+
+                db.SubmitChanges();
+                MessageBox.Show("Cập nhật lớp học thành công!", "Thông báo",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi khi cập nhật: {ex.Message}", "Lỗi",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox4.Text, out int id))
+            {
+                MessageBox.Show("Vui lòng chọn một lớp học để xóa.", "Cảnh báo",
+                                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            LopHoc lh = db.LopHocs.FirstOrDefault(l => l.id == id);
+            if (lh == null)
+            {
+                MessageBox.Show("Không tìm thấy lớp học trong cơ sở dữ liệu.", "Lỗi",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (lh.SinhViens.Any())
+            {
+                MessageBox.Show("Không thể xóa lớp học này vì đang có sinh viên tham gia.", "Lỗi",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (MessageBox.Show("Bạn có chắc chắn muốn xóa lớp học này không?", "Xác nhận",
+                                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                try
+                {
+                    db.LopHocs.DeleteOnSubmit(lh);
+                    db.SubmitChanges();
+                    MessageBox.Show("Xóa lớp học thành công!", "Thông báo",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    btnLamMoi_Click(sender, e);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Lỗi khi xóa: {ex.Message}", "Lỗi",
+                                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnLamMoi_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            txtTimKiem.Clear();
+            searchKeyword = "";
+            currentPage = 1;
+            LoadData();
+        }
+
+        private void dgv_DSLH_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgv_DSLH.Rows[e.RowIndex];
+                textBox4.Text = row.Cells["id"].Value?.ToString();
+                textBox1.Text = row.Cells["malop"].Value?.ToString();
+                textBox2.Text = row.Cells["tenlop"].Value?.ToString();
+                textBox3.Text = row.Cells["ghichu"].Value?.ToString();
+            }
+        }
+
+        private void btnTim_Click(object sender, EventArgs e)
+        {
+            searchKeyword = txtTimKiem.Text.Trim().ToLower();
+            currentPage = 1;
+            LoadData();
+        }
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            currentPage = 1;
+            LoadData();
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            if (currentPage > 1)
+            {
+                currentPage--;
+                LoadData();
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (currentPage < totalPage)
+            {
+                currentPage++;
+                LoadData();
+            }
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            currentPage = totalPage;
+            LoadData();
         }
 
         private void dgv_DSLH_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -38,10 +269,9 @@ namespace CSharp_68PM2_NguyenMinhTien_0025868
 
         }
 
-        private void UCQLSinhVien_Load(object sender, EventArgs e)
+        private void textBox4_TextChanged(object sender, EventArgs e)
         {
-            List<LopHoc> dssv = db.LopHocs.ToList();
-            dgv_DSLH.DataSource = dssv;
+
         }
     }
 }
